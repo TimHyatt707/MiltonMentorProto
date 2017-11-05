8c47d301e63e94c2d9e53b8e4b8bd30fef79d152
TimHyatt707 <timhyatt707@gmail.com>
2017-11-04 18:25:49 -0700

    updated form

--- src/components/SuccessPageComponents/MenteeFormComponent.js
+++ src/components/SuccessPageComponents/MenteeFormComponent.js
@@ -3,6 +3,7 @@ import Paper from "material-ui/Paper";
 import DropDownMenu from "material-ui/DropDownMenu";
 import MenuItem from "material-ui/MenuItem";
 import RaisedButton from "material-ui/RaisedButton";
+import { Link } from "react-router-dom";
 
 export default class MenteeFormComponent extends React.Component {
   constructor(props) {
@@ -13,7 +14,7 @@ export default class MenteeFormComponent extends React.Component {
     return (
       <Paper zdepth={2} style={{ minWidth: 500, maxWidth: 500, margin: 40 }}>
         <h1 style={{ display: "flex", justifyContent: "center" }}>Mentee</h1>
-        <div
+        <form
           style={{
             display: "flex",
             flexDirection: "column",
@@ -21,49 +22,79 @@ export default class MenteeFormComponent extends React.Component {
             alignItems: "center",
             alignContent: "spaceAround"
           }}
+          onSubmit={this._onSubmit}
         >
+          <p>Personality</p>
           <DropDownMenu
+            id="personality"
             value={this.state.valueSet1}
+            style={{ maxWidth: 250, margin: 20 }}
             onChange={this._handleChange1}
-            style={{ maxWidth: 250, margin: 20 }}
           >
-            <MenuItem value={1} label="Personality" primaryText="something" />
-            <MenuItem value={2} label="Personality" primaryText="something" />
-            <MenuItem value={3} label="Personality" primaryText="something" />
-            <MenuItem value={4} label="Personality" primaryText="something" />
+            <MenuItem value={1} primaryText="Introvert" />
+            <MenuItem value={2} primaryText="Extrovert" />
           </DropDownMenu>
+          <p>Career</p>
           <DropDownMenu
+            id="career"
             value={this.state.valueSet2}
-            onChange={this._handleChange2}
             style={{ maxWidth: 250, margin: 20 }}
+            onChange={this._handleChange2}
           >
-            <MenuItem value={1} label="Personality" primaryText="something" />
-            <MenuItem value={2} label="Personality" primaryText="something" />
-            <MenuItem value={3} label="Personality" primaryText="something" />
-            <MenuItem value={4} label="Personality" primaryText="something" />
+            <MenuItem
+              value={1}
+              label="Career Field"
+              primaryText="Engineering"
+            />
+            <MenuItem value={2} primaryText="Business" />
+            <MenuItem value={3} primaryText="Finances" />
+            <MenuItem
+              value={4}
+              label="Career Field"
+              primaryText="Real Estate"
+            />
           </DropDownMenu>
+          <p>Hobbies</p>
           <DropDownMenu
             value={this.state.valueSet3}
-            onChange={this._handleChange3}
+            onChange={this._handleChange}
             style={{ maxWidth: 250, margin: 20 }}
+            onChange={this._handleChange3}
           >
-            <MenuItem value={1} label="Personality" primaryText="something" />
-            <MenuItem value={2} label="Personality" primaryText="something" />
-            <MenuItem value={3} label="Personality" primaryText="something" />
-            <MenuItem value={4} label="Personality" primaryText="something" />
+            <MenuItem value={1} primaryText="Kayaking" />
+            <MenuItem value={2} primaryText="Bowling" />
+            <MenuItem value={3} primaryText="Fishing" />
+            <MenuItem value={4} primaryText="Surfing" />
           </DropDownMenu>
-          <RaisedButton label="GO" style={{ maxWidth: 250, margin: 20 }} />
-        </div>
+          <Link to="/matches">
+            <RaisedButton
+              label="GO"
+              type="submit"
+              style={{ maxWidth: 250, margin: 20 }}
+            />
+          </Link>
+        </form>
       </Paper>
     );
   }
-  _handleChange1 = e => {
-    e.preventDefault();
+  _handleChange1 = (event, index, value) => {
+    this.setState({ valueSet1: value });
   };
-  _handleChange2 = e => {
-    e.preventDefault();
+
+  _handleChange2 = (event, index, value) => {
+    this.setState({ valueSet2: value });
   };
-  _handleChange3 = e => {
+
+  _handleChange3 = (event, index, value) => {
+    this.setState({ valueSet3: value });
+  };
+  _onSubmit = e => {
     e.preventDefault();
+    const personality = e.target.personality.value;
+    const career = e.target.career.value;
+    const hobby = e.target.hobby.value;
+    const request = Object.assign({}, career, personality, hobby);
+    console.log(request);
+    this.props.onSubmit(request);
   };
 }
--- src/components/SuccessPageComponents/MentorFormComponent.js
+++ src/components/SuccessPageComponents/MentorFormComponent.js
@@ -3,6 +3,7 @@ import Paper from "material-ui/Paper";
 import DropDownMenu from "material-ui/DropDownMenu";
 import MenuItem from "material-ui/MenuItem";
 import RaisedButton from "material-ui/RaisedButton";
+import { Link } from "react-router-dom";
 
 export default class MentorFormComponent extends React.Component {
   constructor(props) {
@@ -13,7 +14,7 @@ export default class MentorFormComponent extends React.Component {
     return (
       <Paper zdepth={2} style={{ minWidth: 500, maxWidth: 500, margin: 40 }}>
         <h1 style={{ display: "flex", justifyContent: "center" }}>Mentor</h1>
-        <div
+        <form
           style={{
             display: "flex",
             flexDirection: "column",
@@ -21,40 +22,68 @@ export default class MentorFormComponent extends React.Component {
             alignItems: "center",
             alignContent: "spaceAround"
           }}
+          onSubmit={this._onSubmit}
         >
           <DropDownMenu
+            id="personality"
             value={this.state.valueSet1}
-            onChange={this._handleChange1}
             style={{ maxWidth: 250, margin: 20 }}
+            onChange={this._handleChange}
           >
-            <MenuItem value={1} label="Personality" primaryText="something" />
-            <MenuItem value={2} label="Personality" primaryText="something" />
-            <MenuItem value={3} label="Personality" primaryText="something" />
-            <MenuItem value={4} label="Personality" primaryText="something" />
+            <MenuItem value={1} label="Personality" primaryText="Introvert" />
+            <MenuItem value={2} label="Personality" primaryText="Extrovert" />
           </DropDownMenu>
           <DropDownMenu
+            id="career"
             value={this.state.valueSet2}
-            onChange={this._handleChange2}
             style={{ maxWidth: 250, margin: 20 }}
+            onChange={this._handleChange}
           >
-            <MenuItem value={1} label="Personality" primaryText="something" />
-            <MenuItem value={2} label="Personality" primaryText="something" />
-            <MenuItem value={3} label="Personality" primaryText="something" />
-            <MenuItem value={4} label="Personality" primaryText="something" />
+            <MenuItem
+              value={1}
+              label="Career Field"
+              primaryText="Engineering"
+            />
+            <MenuItem value={2} label="Career Field" primaryText="Business" />
+            <MenuItem value={3} label="Career Field" primaryText="Finances" />
+            <MenuItem
+              value={4}
+              label="Career Field"
+              primaryText="Real Estate"
+            />
           </DropDownMenu>
           <DropDownMenu
             value={this.state.valueSet3}
-            onChange={this._handleChange3}
+            onChange={this._handleChange}
             style={{ maxWidth: 250, margin: 20 }}
+            onChange={this._handleChange}
           >
-            <MenuItem value={1} label="Personality" primaryText="something" />
-            <MenuItem value={2} label="Personality" primaryText="something" />
-            <MenuItem value={3} label="Personality" primaryText="something" />
-            <MenuItem value={4} label="Personality" primaryText="something" />
+            <MenuItem value={1} label="Hobbies" primaryText="Kayaking" />
+            <MenuItem value={2} label="Hobbies" primaryText="Bowling" />
+            <MenuItem value={3} label="Hobbies" primaryText="Fishing" />
+            <MenuItem value={4} label="Hobbies" primaryText="Surfing" />
           </DropDownMenu>
-          <RaisedButton label="GO" style={{ maxWidth: 250, margin: 20 }} />
-        </div>
+          <Link to="/matches">
+            <RaisedButton
+              label="GO"
+              type="submit"
+              style={{ maxWidth: 250, margin: 20 }}
+            />
+          </Link>
+        </form>
       </Paper>
     );
   }
+  _handleChange = (event, index, value) => {
+    this.setState({ value });
+  };
+  _onSubmit = e => {
+    e.preventDefault();
+    const personality = e.target.personality.value;
+    const career = e.target.career.value;
+    const hobby = e.target.hobby.value;
+    const request = Object.assign({}, career, personality, hobby);
+    console.log(request);
+    this.props.onSubmit(request);
+  };
 }
